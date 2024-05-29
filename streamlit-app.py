672a9c5bd2416d0a793814c0673021a450328235
saif-corgi <119037587+saif-corgi@users.noreply.github.com>
2024-05-29 22:16:54 +0800

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -41,7 +41,9 @@ st.markdown(
 )
 
 def main():
-    st.title("Stripe Metrics Dashboard Generator")
+    col1, col2, col3 = st.columns([1,1,1])
+    with col2:
+        st.title("Stripe Metrics Dashboard Generator")
     api_key = st.text_input("Enter your Stripe API key:", type="password")
 
     if api_key:
