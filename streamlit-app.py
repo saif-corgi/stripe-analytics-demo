8cc06bf4d8ae2c7a111a86625293ac4db68fea07
saif-corgi <119037587+saif-corgi@users.noreply.github.com>
2024-05-30 10:00:50 +0800

    Update streamlit-app.py

--- streamlit-app.py
+++ streamlit-app.py
@@ -35,7 +35,10 @@ with col2:
 # Streamlit app to input Stripe API key and generate dashboard
 def main():
     st.markdown("<h1 style='text-align: center; color: black;'>Corgi Metrics Preview</h1>", unsafe_allow_html=True)
-    api_key = st.text_input("Enter your API key:", type="password", help="Please enter your API key to access the dashboard.", key="api_key_input")
+    api_key = st.text_input("<h3 style='color: black;'>Enter your API key:</h3>", type="password", help="Please enter your API key to access the dashboard.", key="api_key_input")
+    
+    # "<h3 style='color: black;'>Enter your API key:</h3>"
+    
     if api_key:
         stripe.api_key = api_key
         try:
